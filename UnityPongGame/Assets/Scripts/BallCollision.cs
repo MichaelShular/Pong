3800332a605ff72036e0b638a0a762fb7e4c2c58
Mick <mickx.shular@gmail.com>
2021-06-19 20:35:35 -0400

    Ball collision reflection

---
+++ UnityPongGame/Assets/Scripts/BallCollision.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallCollision : MonoBehaviour
+{
+    private Rigidbody2D body;
+    private Vector3 lastVel;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lastVel = body.velocity;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        float speed = lastVel.magnitude;
+
+        Vector3 dir = Vector3.Reflect(lastVel.normalized, collision.GetContact(0).normal);
+        body.velocity = dir * speed;
+    
+    }
+}
--- UnityPongGame/Assets/Scripts/PlayerControl.cs
+++ UnityPongGame/Assets/Scripts/PlayerControl.cs
@@ -5,12 +5,18 @@ using UnityEngine;
 public class PlayerControl : MonoBehaviour
 {
     [SerializeField] float lockedXPosition = -40.0f;
+    Rect screenRect = new Rect(0, 0, Screen.width, Screen.height);
     void Update()
     {
-        //Setting the mouse postion to the players postion
-        transform.position = Camera.main.ScreenToWorldPoint( new Vector3(Input.mousePosition.x, Input.mousePosition.y, 1));
-        //Disabling the x movement 
-        transform.position = new Vector2(lockedXPosition, transform.position.y); 
+        //if (transform.position.y <= 23 && transform.position.y >= -23)
+        if (screenRect.Contains(Input.mousePosition))
+        {
+            //Setting the mouse postion to the players postion
+            transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 1));
+            //Disabling the x movement 
+            transform.position = new Vector2(lockedXPosition, transform.position.y);
+        }
+      
     }
 
 }
