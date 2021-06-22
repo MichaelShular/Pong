a993e5e449cbe1cbc052d116e550aadd8b4aa042
Mick <mickx.shular@gmail.com>
2021-06-21 23:46:43 -0400

    Pause Menu

---
+++ UnityPongGame/Assets/Scripts/GameUIControl.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameUIControl : MonoBehaviour
+{
+    [SerializeField] private Canvas pauseCanvas;
+    [SerializeField] private GameObject playerPaddle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            Time.timeScale = 0;
+            pauseCanvas.enabled = true;
+            playerPaddle.GetComponent<PlayerControl>().enabled = false;
+        }
+    }
+
+    public void PauseButton()
+    {
+        Time.timeScale = 1;
+        pauseCanvas.enabled = false;
+        playerPaddle.GetComponent<PlayerControl>().enabled = true;
+
+    }
+    public void GoToMainMenu()
+    {
+        SceneManager.LoadScene("Main_Menu");
+    }
+}
